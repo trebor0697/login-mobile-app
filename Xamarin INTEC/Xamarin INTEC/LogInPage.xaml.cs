a98eee8bd9cb532fc23971a4ed13941f9be4b2ec
trebo <trebo@DESKTOP-NJNIPF9>
2021-08-20 22:08:50 +0800

    Add project files.

---
+++ Xamarin INTEC/Xamarin INTEC/LogInPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_INTEC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogInPage : ContentPage
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void ButtonClicked1(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty (emailEntry.Text) || String.IsNullOrEmpty(passwordEntry.Text))
+            {
+                await DisplayAlert("Error", "Campos email y/o contraseña no pueden estar vacío", "Ok");
+
+            }
+            else 
+            {
+                await DisplayAlert("Bienvenido", "Hola " + emailEntry.Text, "Ok");
+                await Navigation.PushModalAsync(new HomePage());
+
+            }
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new RegistrationPage());
+        }
+    }
+}
---
+++ Xamarin INTEC/Xamarin INTEC/RegistrationPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_INTEC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void ButtonClicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(name.Text) || String.IsNullOrEmpty(email.Text) || String.IsNullOrEmpty(password.Text) || String.IsNullOrEmpty(passwordConfirm.Text))
+            {
+                await DisplayAlert("Error", "Ningun campo puede estar vacio", "Ok");
+
+            }
+            else if (password.Text != passwordConfirm.Text)
+            {
+                await DisplayAlert("Error", "Las contraseñas deben ser iguales", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Bienvenido", "Hola " + name.Text, "Ok");
+                await Navigation.PushModalAsync(new HomePage());
+            }
+        }
+    }
+}
